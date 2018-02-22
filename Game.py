0a6d47c2bccfb7fa39344cf0d3172460800279ad
TPC9000 <adam.mabin@gmail.com>
2018-02-22 10:25:58 +0000

    Change game.py

--- Game.py
+++ Game.py
@@ -5,20 +5,7 @@ try:
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-import math
-
-# Classes
-
-class GameInstance:
-
-    def drawWindow(self, canvas): pass
-
-    def __init__(self, windowSize):
-        window = simplegui.create_frame("Game Name", windowSize.x, windowSize.y)
-        window.set_draw_handler(self.drawWindow)
-        self.window = window
-
-
+import math, Level, StartMenu, Vector, GameInstance
 
 # Constants
 
