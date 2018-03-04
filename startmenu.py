fb8813fd9ea7a8dc82899852332207d4d2427190
Adam Mabin <adam.mabin@googlemail.com>
2018-03-04 21:24:07 +0000

    Add more buttons for testing

--- startmenu.py
+++ startmenu.py
@@ -7,8 +7,13 @@ class StartMenu(WindowHandler):
 
     def __init__(self, window: Window):
         super().__init__(window)
-        self.btn_start = Button("Start Game", (200, 50), (50, 50), 3, Color(255, 0, 0), Color(127, 0, 0), Color(0, 190, 0), Color(190, 0, 0), 0.05)
+        self.btn = []
+
+        #Args: text, font size, font face (either monospace, serif or sans-serif), position, size, border size, back colour normal, text colour normal, back colour mouse over, text colour mouse over, lerp factor (bigger = more aggressive change)
+        for x in range(4):
+            self.btn.append(Button("Test" + str(x), 16 + 8 * (x + 1), "serif", (200 * (x + 1), 50), (100, 50), 3, Color(255, 0, 0), Color(127, 0, 0), Color(0, 190, 0), Color(190, 0, 0), 0.05))
 
     def render(self, canvas: simplegui.Canvas):
-        self.btn_start.render(canvas)
+        for button in self.btn:
+            button.render(canvas)
         
