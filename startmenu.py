76d5f80cf83fa92d255a31fa840b8e43e1edd9f1
Adam Mabin <adam.mabin@googlemail.com>
2018-03-04 19:25:19 +0000

    Change start menu test button

--- startmenu.py
+++ startmenu.py
@@ -7,7 +7,8 @@ class StartMenu(WindowHandler):
 
     def __init__(self, window: Window):
         super().__init__(window)
-        self.btn_start = Button("Start Game", (200, 50), (50, 50), Color(255, 0, 0), Color(0, 0, 255))
+        self.btn_start = Button("Start Game", (200, 50), (50, 50), 3, Color(255, 0, 0), Color(127, 0, 0), Color(0, 190, 0), Color(190, 0, 0), 0.05)
 
     def render(self, canvas: simplegui.Canvas):
         self.btn_start.render(canvas)
+        
