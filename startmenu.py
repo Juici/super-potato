254aa73271aada53aee02f31de523cdf5b4e7056
Kane <k.crichton96@gmail.com>
2018-03-05 15:30:51 +0000

    small changes

--- startmenu.py
+++ startmenu.py
@@ -2,12 +2,14 @@ from util import simplegui
 from window import WindowHandler
 import button
 
+bg_image = simplegui.load_image()
+
 class StartMenu(WindowHandler):
 
     def __init__(self):
 
-    	self.btn_start = button("Start Game", Vector(200, 50), Vector(50, 50), "Red", "Blue")
+        self.btn_start = button("Start Game", Vector(200, 50), Vector(50, 50), "Red", "Blue")
 
     def render(self):
 
-    	self.btn_start().update()
+        self.btn_start().update()
