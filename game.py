22c7f9a2dea849c61532f59b280f98d4b40553c2
TPC9000 <adam.mabin@gmail.com>
2018-03-08 09:53:49 +0000

    Modify game.py

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 from constants import *
 from window import Window
 from startmenu import StartMenu
+from level import Level
+from character import Character
 
 
 def main():
@@ -8,7 +10,10 @@ def main():
     The main launch function.
     """
     window = Window(GAME_NAME, WINDOW_SIZE, controls=SHOW_CONTROL_PANEL, fullscreen=FULLSCREEN)
-    window.handler = StartMenu(window)
+    playerchar = Character("noimage", window)
+    first_level = Level(window)
+    first_level.set_character(window)
+    window.handler = first_level
     window.show()
 
 
