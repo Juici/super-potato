b65ae79283669212eb1d38a66bf2346fbb79eab7
TPC9000 <adam.mabin@gmail.com>
2018-03-09 16:48:28 +0000

    Add velocity, gravity, basic floor and wall collision

--- character.py
+++ character.py
@@ -10,35 +10,55 @@ class Character(Renderable):
         self.current_position = Vector(100, 200)
         self.window_size = window.get_size()
         self.target_move_x = 0
+        self.target_move_y = 0
+        self.force_down_y = 0 # Keep 0 on ground collision, else set to 1 and multiply by 2 each frame
+        self.on_ground = False
+        self.jumping = False
+        self.gravity = 0.2
 
-    def get_desired_movement() -> Vector:
-        return Vector(self.target_move_x, 0)
+    def extrapolate(self) -> Vector:
+        return self.current_position + Vector(self.target_move_x, self.target_move_y)
 
     def on_key_down(self, key: Key):
+        if (key == Key.SPACE):
+            self.jumping = True
         if (key == Key.KEY_A):
-            print("Down A")
             self.target_move_x -= 1
         elif (key == Key.KEY_D):
-            print("Down D")
             self.target_move_x += 1
 
     def on_key_up(self, key: Key):
-        print("A key up")
+        if (key == Key.SPACE):
+            self.jumping = False
         if (key == Key.KEY_A):
-            print("Up A")
             self.target_move_x += 1
         elif (key == Key.KEY_D):
-            print("Up D")
             self.target_move_x -= 1
 
-    def get_pos() -> Vector:
+    def get_pos(self) -> Vector:
         return self.current_position
 
     def render(self, canvas: simplegui.Canvas, renderables: Renderable):
-        #canvas.draw_image()
-        #self.sprite.render(canvas)
-        #print(self.target_move_x)
-        temp_position = self.current_position + Vector(self.target_move_x, 0)
+        temp_position = self.extrapolate()
+
         if temp_position.x <= self.window_size[0] and temp_position.x >= 0:
-            self.current_position = temp_position
+            self.current_position.x = temp_position.x
+
+        if temp_position.y <= self.window_size[1] and temp_position.y >= 0:
+            self.on_ground = False
+            self.force_down_y += self.gravity
+        else:
+            self.force_down_y = self.window_size[1]
+            self.current_position.y = 0
+            self.on_ground = True
+
+        print(self.on_ground)
+        print(self.jumping)
+
+        if self.jumping and self.on_ground:
+            print("OK")
+            self.force_down_y = -5
+
+        self.current_position.y += self.force_down_y
+        
         canvas.draw_text("C", (self.current_position.x, self.current_position.y), 10, "Red")
