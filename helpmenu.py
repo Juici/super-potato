795baa0b256c8c02c4038909843068047ce21e34
Jamie <jamie.snellock@gmail.com>
2018-03-09 16:47:32 +0000

    The new helpmenu for the game.

--- helpmenu.py
+++ helpmenu.py
@@ -1,22 +1,19 @@
-from util import simplegui, Color
+from util import simplegui
 from window import Window, WindowHandler
 from button import Button
 
-class helpMenu(WindowHandler):
-    def __init__(self, window, Window):
-        super().__init__(window)
-        self.btn_backToMenu = Button("Back to main menu", (200, 50), (50, 50), Color(255,0,0), Color(0, 0, 255))
+class HelpMenu(WindowHandler):
 
-    def draw_handler(simplegui.Canvas):
-        #Requires source image centre and source image height/width.
-        canvas.draw_image(image, (), (), (50, 50), (100, 100))
-    #Requires address of image. Intended to display an image detailing the control scheme.
-    image = simplegui.load_image()
+    def __init__(self, window: Window):
+        super().__init__(window)
+
+        btn_back = Button("Back to main menu", (200, 50), (50, 50), Color(255,0,0), Color(0, 0, 255))
+        self.children.append(btn_back)
 
     def render(self, canvas: simplegui.Canvas):
-        self.btn_backToMenu.render(canvas)
+        super().render(canvas)
 
-    #May need to be altered/moved.
-    frame = simplegui.create_frame('Display', 100, 100)
-    frame.set_draw_handler(draw_handler)
-    frame.start()
+        canvas.draw_text('Keys:', (20,20), 24, 'Black')
+        canvas.draw_text('D - Go forwards', (20, 40), 24, 'Black')
+        canvas.draw_text('A - Go backwards', (20, 60), 24, 'Black')
+        canvas.draw_text('Space bar - Jump', (20, 80), 24, 'Black')
