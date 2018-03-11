0adebb12296a16c7afa0b26e071f804a2aa4ff25
Adam Mabin <adam.mabin@googlemail.com>
2018-03-11 21:47:50 +0000

    Modify adam_game.py

--- adam_game.py
+++ adam_game.py
@@ -3,6 +3,8 @@ from startmenu import StartMenu
 from window import Window
 from level import Level
 from character import Character
+from block import Block
+from vector import Vector
 
 
 def main():
@@ -10,9 +12,11 @@ def main():
     The main launch function.
     """
     window = Window(GAME_NAME, WINDOW_SIZE, fullscreen=FULLSCREEN)
-    playerchar = Character("noimage", window)
     first_level = Level(window)
-    first_level.set_character(playerchar)
+    playerchar = Character(Vector(100, 100), Vector(100, 100), window, first_level)
+    block_one = Block(Vector(300, 550), Vector(100, 10), "rgb(127, 127, 127)", window, first_level)
+    block_two = Block(Vector(420, 520), Vector(100, 10), "rgb(127, 127, 127)", window, first_level)
+    block_three = Block(Vector(540, 420), Vector(300, 10), "rgb(127, 127, 127)", window, first_level)
     window.handler = first_level
     window.show()
 
