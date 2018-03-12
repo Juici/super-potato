83abcad5966952ec8e713b8fb528be6a92423c8e
Kane <k.crichton96@gmail.com>
2018-03-12 12:42:41 +0000

    Fixed a bug with the background, should run now

--- startmenu.py
+++ startmenu.py
@@ -6,7 +6,7 @@ from button import Button
 from constants import *
 from levelone import LevelOne
 
-START_BUTTON_SIZE = Vector(200, 50)
+BUTTON_SIZE = Vector(200, 50)
 IMAGE_SIZE = Vector(1000, 300)
 
 bg_image = simplegui.load_image('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/0kjHIH6/old-retro-video-game-arcade-clouds-moving-on-a-blue-sky_hcd0pxim__F0000.png')
@@ -19,7 +19,13 @@ class StartMenu(WindowHandler):
                  #border_size: int = 0, font: Font = Font('sans-serif', 15)
 
     def handle_btn_1_click(self, pos: Vector, dunno):
+        print ("Start button clicked")
         self.window.destroy()
+
+    def handle_btn_2_click(self, pos: Vector, dunno):
+        #load Jamie's help class
+        print ("Help button clicked")
+        pass
 
     def __init__(self, window: Window):
         super().__init__(window)
@@ -29,9 +35,13 @@ class StartMenu(WindowHandler):
         self.window = window
 
         # Template to create new button
-        btn1 = Button(window, "Start Game", Vector(win_size[0] / 2 - START_BUTTON_SIZE.x / 2, 100), START_BUTTON_SIZE, Color(255, 0, 0), Color(0, 255, 0), Color(0, 255, 0), Color(255, 0, 0), 0)
+        btn1 = Button(window, "Start Game", Vector(win_size[0] / 2 - BUTTON_SIZE.x / 2, 300), BUTTON_SIZE, Color(255, 0, 0), Color(0, 255, 0), Color(0, 255, 0), Color(255, 0, 0), 0)
         btn1.set_click_handler(self.handle_btn_1_click)
         self.btns.append(btn1)
+
+        btn2 = Button(window, "Help", Vector(win_size[0] / 2 - BUTTON_SIZE.x / 2 , 375), BUTTON_SIZE, Color(255, 0, 0), Color(0, 255, 0), Color(0, 255, 0), Color(255, 0, 0), 0)
+        btn2.set_click_handler(self.handle_btn_2_click)
+        self.btns.append(btn2)
 
     def render(self, canvas: simplegui.Canvas):
         canvas.draw_image(bg_image, (1920 / 2, 1080 / 2), (1920, 1080), (500, 300), WINDOW_SIZE)
