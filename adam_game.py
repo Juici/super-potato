de32df5a6f094e5e3c15aa106ebfa80e8d80961b
Adam Mabin <adam.mabin@googlemail.com>
2018-03-12 18:41:55 +0000

    Modify adam_game.py

--- adam_game.py
+++ adam_game.py
@@ -1,7 +1,7 @@
 from constants import FULLSCREEN, GAME_NAME, WINDOW_SIZE
 from startmenu import StartMenu
 from window import Window
-from level import Level
+from level import GameView
 from level_items import *
 from vector import Vector
 
@@ -11,22 +11,52 @@ def main():
     """
     window = Window(GAME_NAME, WINDOW_SIZE, fullscreen=FULLSCREEN)
     
-    level_one = Level(window)
-    level_two = Level(window)
+    level_two = GameView(window, None)
+    level_one = GameView(window, level_two)
 
-    playerchar = Character(Vector(100, 100), Vector(10, 10), window, level_one)
+    l1_player_character    = Character(window, Vector(100, 100), Vector(10, 10))
+    l1_trap_one            = Trap(window, Vector(1200, 270), Vector(80, 30), "rgb(180, 16, 16)")
+    l1_finish_one          = Finish(window, Vector(1580, 180), Vector(40, 120), "rgb(255, 255, 255)")
+    l1_platform_start      = Platform(window, Vector(50, 550), Vector(200, 10), "rgb(127, 127, 127)")
+    l1_platform_one        = Platform(window, Vector(300, 550), Vector(100, 10), "rgb(127, 127, 127)")
+    l1_platform_two        = Platform(window, Vector(420, 485), Vector(100, 10), "rgb(127, 127, 127)")
+    l1_platform_three      = Platform(window, Vector(540, 420), Vector(300, 10), "rgb(127, 127, 127)")
+    l1_platform_four       = Platform(window, Vector(940, 380), Vector(15, 10), "rgb(127, 127, 127)")
+    l1_platform_five       = Platform(window, Vector(940, 300), Vector(15, 10), "rgb(127, 127, 127)")
+    l1_platform_six        = Platform(window, Vector(1020, 300), Vector(600, 10), "rgb(127, 127, 127)")
 
-    platform_start = Platform(Vector(50, 550), Vector(200, 10), "rgb(127, 127, 127)", window, level_one)
-    platform_one = Platform(Vector(300, 550), Vector(100, 10), "rgb(127, 127, 127)", window, level_one)
-    platform_two = Platform(Vector(420, 485), Vector(100, 10), "rgb(127, 127, 127)", window, level_one)
-    platform_three = Platform(Vector(540, 420), Vector(300, 10), "rgb(127, 127, 127)", window, level_one)
-    platform_four = Platform(Vector(940, 380), Vector(15, 10), "rgb(127, 127, 127)", window, level_one)
-    platform_five = Platform(Vector(940, 300), Vector(15, 10), "rgb(127, 127, 127)", window, level_one)
-    platform_six = Platform(Vector(1020, 300), Vector(600, 10), "rgb(127, 127, 127)", window, level_one)
+    level_one.add_child(l1_player_character)
+    level_one.add_child(l1_platform_start)
+    level_one.add_child(l1_platform_one)
+    level_one.add_child(l1_platform_two)
+    level_one.add_child(l1_platform_three)
+    level_one.add_child(l1_platform_four)
+    level_one.add_child(l1_platform_five)
+    level_one.add_child(l1_platform_six)
+    level_one.add_child(l1_trap_one)
+    level_one.add_child(l1_finish_one)
 
-    trap_one = Trap(Vector(1200, 270), Vector(80, 30), "rgb(180, 16, 16)", window, level_one)
+    l2_player_character    = Character(window, Vector(100, 100), Vector(10, 10))
+    l2_trap_one            = Trap(window, Vector(1200, 270), Vector(80, 30), "rgb(180, 16, 16)")
+    l2_finish_one          = Finish(window, Vector(1580, 180), Vector(40, 120), "rgb(255, 255, 255)")
+    l2_platform_start      = Platform(window, Vector(50, 550), Vector(200, 10), "rgb(127, 127, 127)")
+    l2_platform_one        = Platform(window, Vector(300, 550), Vector(100, 10), "rgb(127, 127, 127)")
+    l2_platform_two        = Platform(window, Vector(420, 485), Vector(100, 10), "rgb(127, 127, 127)")
+    l2_platform_three      = Platform(window, Vector(540, 420), Vector(300, 10), "rgb(127, 127, 127)")
+    l2_platform_four       = Platform(window, Vector(940, 380), Vector(15, 10), "rgb(127, 127, 127)")
+    l2_platform_five       = Platform(window, Vector(940, 300), Vector(15, 10), "rgb(127, 127, 127)")
+    l2_platform_six        = Platform(window, Vector(1020, 300), Vector(600, 10), "rgb(127, 127, 127)")
 
-    finish_one = Finish(Vector(1580, 180), Vector(40, 120), "rgb(255, 255, 255)", window, level_one)
+    level_two.add_child(l2_player_character)
+    level_two.add_child(l2_trap_one)
+    level_two.add_child(l2_platform_one)
+    level_two.add_child(l2_platform_two)
+    level_two.add_child(l2_platform_three)
+    level_two.add_child(l2_platform_four)
+    level_two.add_child(l2_platform_five)
+    level_two.add_child(l2_platform_six)
+    level_two.add_child(l2_trap_one)
+    level_two.add_child(l2_finish_one)
 
     window.handler = level_one
     window.show()
