41157e8b4ead756c7c2f5bc27095b2e940be2b35
Adam Mabin <adam.mabin@googlemail.com>
2018-03-12 08:38:33 +0000

    Change adam_game.py

--- adam_game.py
+++ adam_game.py
@@ -13,7 +13,7 @@ def main():
     """
     window = Window(GAME_NAME, WINDOW_SIZE, fullscreen=FULLSCREEN)
     first_level = Level(window)
-    playerchar = Character(Vector(100, 100), Vector(100, 100), window, first_level)
+    playerchar = Character(Vector(100, 100), Vector(10, 10), window, first_level)
     block_one = Block(Vector(300, 550), Vector(100, 10), "rgb(127, 127, 127)", window, first_level)
     block_two = Block(Vector(420, 520), Vector(100, 10), "rgb(127, 127, 127)", window, first_level)
     block_three = Block(Vector(540, 420), Vector(300, 10), "rgb(127, 127, 127)", window, first_level)
