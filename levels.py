9c5aedf8ab59b342a1961d524d88ab3ace202819
TPC9000 <adam.mabin@gmail.com>
2018-03-15 10:51:41 +0000

    Change levels.py

--- levels.py
+++ levels.py
@@ -3,12 +3,14 @@ import simplegui
 from typing import List, TYPE_CHECKING
 from geom import Vector
 from level_items import LevelItem
+from constants import LEVEL_BACKGROUND_IMAGE, LEVEL_BACKGROUND_DIMS, LEVEL_BACKGROUND_STRETCH_X, WINDOW_SIZE
+from util import load_image
 
 # Work around cyclic imports.
 if TYPE_CHECKING:
     from world import World
 
-__all__ = ['Level', 'LEVELS']
+__all__ = ['Level']
 
 
 class Level(object):
@@ -26,6 +28,8 @@ class Level(object):
         self.items: List[LevelItem] = []
         self.finished = False
 
+        self.parallax = load_image(LEVEL_BACKGROUND_IMAGE)
+
     def add_item(self, item: LevelItem):
         """
         Adds the item to the level.
@@ -42,29 +46,29 @@ class Level(object):
         """
         Called on every game tick to render the level.
         """
+
+        # Draw parallax background, todo
+        bg_size = (self.parallax.get_width(), self.parallax.get_height())
+        bg_center = (bg_size[0] / 2, bg_size[1] / 2)
+        window_center = (-self.offset.x, WINDOW_SIZE[1] / 2)
+        canvas.draw_image(self.parallax, bg_center, bg_size, window_center, (LEVEL_BACKGROUND_STRETCH_X, WINDOW_SIZE[1]))
+
+        # Render items
         for item in self.items:
             item.render(canvas)
+
+        # Render player
+        world.player.render(canvas)
 
         # Add the level scroll, mutating the current offset.
         self.offset.add(self.scroll)
 
         # Load next level.
         if self.finished:
+            levels = self.world.levels
+
             next_level = self.level + 1
-            if len(LEVELS) >= next_level:
-                world.level = LEVELS[next_level - 1]
+            if len(levels) >= next_level:
+                world.level = levels[next_level - 1]
             else:
                 world.level = None
-
-
-LEVELS: List[Level] = []
-
-
-def _init_levels():
-    level1 = Level(1, Vector(100, 100))
-    LEVELS.append(level1)
-
-
-_init_levels()
-
-del _init_levels
